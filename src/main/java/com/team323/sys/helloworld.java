bc21e8c12dda40fb10efb4c481c8029da211b267
nomadzwc <burning98@outlook.com>
2019-01-09 16:43:51 +0800

    测试文件

--- src/main/java/com/team323/sys/helloworld.java
+++
@@ -1,8 +0,0 @@
-package com.team323.sys;
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("你好github!!!");
-        System.out.println("再试一次");
-    }
-}
